cd484d081ba3afc091b92268bccc63d225f60e52
mateus-clasen <mateushc2@hotmail.com>
2024-04-20 12:45:23 -0300

    getting started

---
+++ app/src/main/java/com/clasenmateus/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.clasenmateus.weatherapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.clasenmateus.weatherapp.presentation.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                
+            }
+        }
+    }
+}
